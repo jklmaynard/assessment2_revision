45fb6bcf154698597847c252d4d9b5bafb06a4ea
James Maynard <jameskirkmaynard@gmail.com>
2015-02-02 20:05:18 +0000

    Delete in place routes and commit out post methods.

--- app.rb
+++ app.rb
@@ -19,32 +19,3 @@ post('/contacts') do
   erb(:index)
 end
 
-post('/numbers') do
-
-  home = params.fetch('home')
-  @number = Phone.new(home)
-  @number.save()
-  @contact = Contact.find(params.fetch('contact_id').to_i)
-  @contact.add_number(@number)
-
-  mobile = params.fetch('mobile')
-  @mobile = Phone.new(mobile)
-  @mobile.save()
-  @contact.add_number(@mobile)
-
-  work = params.fetch('work')
-  @work = Phone.new(work)
-  @work.save()
-  @contact.add_number(@work)
-  erb(:phones)
-end
-
-get('/numbers/:id') do
-  @number = Phone.find(params.fetch('id').to_i())
-  erb(:phones)
-end
-
-get('/contacts/:id')do
-  @contact = Contact.find(params.fetch('id').to_i())
-  erb(:contacts)
-end
